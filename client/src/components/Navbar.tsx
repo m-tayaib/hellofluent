b3b12502ae9113c45cc8896017c83d446bc62a90
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-08 17:31:41 +0500

    build navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,18 +4,25 @@ import { Links } from "../Types/NavbarTypes"
 
 function Navbar() {
     return (
-        <nav>
+        <nav className="fixed top-0 left-0 w-full h-16 bg-white shadow-sm flex items-center justify-between px-8 z-40">
             {/* logo  */}
-            <div>
-                <SiSololearn />
-                <h3>hellofluent</h3>
+            <div className="flex items-center space-x-2">
+                <SiSololearn className="text-2xl text-logo" />
+                <h3 className="text-xl font-bold text-primary capitalize">HelloFluent</h3>
             </div>
             {/* navlinks  */}
             <div>
-                <ul>
-                    {Links.map((link) => {
-                        return <Link to={link.route} key={link.id}>{link.title}</Link>;
-                    })}
+                <ul className="flex space-x-6">
+                    {Links.map((link) => (
+                        <li key={link.id}>
+                            <Link
+                                to={link.route}
+                                className="text-secondary capitalize  hover:text-primary  transition-colors duration-200 font-medium"
+                            >
+                                {link.title}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </nav>
