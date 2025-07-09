bc5e282e612468aa63b6c6de7c4d5c41204af38e
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-09 13:02:24 +0500

    add margin and import some components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,29 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loading from "./components/Loading";
 
+const Navbar = lazy(() => import("./components/Navbar"))
 const Home = lazy(() => import("./Pages/Home"));
+const Languages = lazy(() => import("./Pages/Languages"))
+const About = lazy(() => import("./Pages/About"))
 // Add more lazy imports as needed
-// const About = lazy(() => import("./Pages/About"));
 
 function App() {
   return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* Add more routes here */}
-      </Routes>
-    </Suspense>
+    <>
+    <Navbar />
+    <div className="pt-16  ">
+       
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/languages" element={<Languages />} />
+            <Route path="/about" element={<About />} />
+            {/* Add more routes here */}
+          </Routes>
+        </Suspense>
+       
+    </div>
+  </>
   );
 }
 
