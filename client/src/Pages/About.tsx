3de16b865076dec4b5fb275474595d6c35094613
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-09 13:01:02 +0500

    add content for comming soon.

---
+++ client/src/Pages/About.tsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#f5f7fa]">
+      <div className="w-[300px] max-w-[90%]">
+
+      </div>
+      <h1 className="mt-8 text-4xl font-bold text-[#222] tracking-wider capitalize">
+        About Page Coming Soon
+      </h1>
+      <p className="mt-4 text-lg text-gray-600 text-center max-w-xl">
+        We're currently working hard to bring you more information about HelloFluent, our mission, and our team.     </p>
+    </div>
+  )
+}
+
+export default About
---
+++ client/src/Pages/Languages.tsx
@@ -0,0 +1,20 @@
+ 
+ 
+
+const Languages = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#f5f7fa]">
+      <div className="w-[300px] max-w-[90%]">
+         
+      </div>
+      <h1 className="mt-8 text-4xl font-bold text-[#222] tracking-wider capitalize">
+        Languages Page Coming Soon
+      </h1>
+      <p className="mt-4 text-lg text-gray-600 text-center max-w-xl">
+        We're actively working to bring you a wide selection of languages and engaging learning experiences. 
+      </p>
+    </div>
+  );
+};
+
+export default Languages;
