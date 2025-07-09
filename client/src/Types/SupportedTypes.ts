c604aa23d53e7b61debcfe3eef6026e5c01b84d9
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-09 16:27:42 +0500

    create supported types component

---
+++ client/src/Types/SupportedTypes.ts
@@ -0,0 +1,25 @@
+type StatsItem = {
+    id : number,
+    title: string;
+    description: string;
+  };
+  
+  export const statsSectionData: StatsItem[] = [
+    {
+      id: 1,
+      title: "Global Learners",
+      description: "Learn from anywhere."
+    },
+    {
+      id: 2,
+      title: "Fluency First",
+      description: "Build real confidence."
+    },
+    {
+      id: 3,
+      title: "Anytime Practice",
+      description: "Quick lessons daily."
+    }
+  ];
+  
+  
