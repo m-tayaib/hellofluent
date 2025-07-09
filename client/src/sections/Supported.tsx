162120122e6bf58f7d41b45695cb0e918cb404f2
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-09 16:27:53 +0500

    buil supported  UI

---
+++ client/src/sections/Supported.tsx
@@ -0,0 +1,13 @@
+import ShowSupport from "../components/ShowSupport"
+
+
+function Supported() {
+  return (
+    <section className="md:px-16 px-8 bg-bgSecondary py-10">
+    <ShowSupport/>
+
+    </section>
+  )
+}
+
+export default Supported
