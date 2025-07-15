07916b2f3c121cf61ba315d1cb231f950e3287ce
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-15 12:54:29 +0500

    build ui for language hero section

--- client/src/Pages/Languages.tsx
+++ client/src/Pages/Languages.tsx
@@ -1,18 +1,12 @@
- 
- 
+import LanguageHero from "../sections/LanguageHero";
+
+
+
 
 const Languages = () => {
   return (
-    <div className="min-h-screen text-center   px-8 md:px-16 flex flex-col items-center justify-center bg-[#f5f7fa]">
-      <div className="w-[300px] max-w-[90%]">
-         
-      </div>
-      <h1 className="mt-8 text-4xl font-bold text-[#222] tracking-wider capitalize">
-        Languages Page Coming Soon
-      </h1>
-      <p className="mt-4 text-lg text-gray-600 text-center max-w-xl">
-        We're actively working to bring you a wide selection of languages and engaging learning experiences. 
-      </p>
+    <div className="">
+      <LanguageHero />
     </div>
   );
 };
---
+++ client/src/sections/LanguageHero.tsx
@@ -0,0 +1,35 @@
+
+
+function LanguageHero() {
+    return (
+        <section
+            className="relative w-full   h-[70%] md:min-h-screen px-2 xs:px-4 sm:px-8 md:px-16 flex items-center justify-center bg-cover bg-no-repeat bg-bottom "
+            style={{
+                backgroundImage: `url('/assets/language_hero_img.jpg')`,
+            }}
+        >
+            {/* Overlay */}
+            <div
+                className="absolute inset-0 z-0"
+                style={{
+                    background: "linear-gradient(to bottom, rgba(25,23,22,0.85) 0%, rgba(25,23,22,0.7) 60%, rgba(25,23,22,0.4) 100%)"
+                }}
+            />
+            {/* Content */}
+            <div className="relative z-10 flex flex-col items-center justify-center text-center px-2 xs:px-4 py-10 sm:py-20 w-full">
+                <h1
+                    className="text-xs xs:text-base sm:text-3xl text-shadow-2xs bg-gradient-to-r from-white to-logo bg-clip-text text-transparent   md:text-6xl lg:text-7xl font-extrabold capitalize leading-tight mb-4 sm:mb-6"
+                >
+                    Start Learning a Language Today
+                </h1>
+                <p
+                    className="text-xs xs:text-sm sm:text-base text-shadow-2xs   md:text-2xl max-w-xs sm:max-w-2xl text-white drop-shadow-md"
+                >
+                    Choose your language and begin practicing with real content.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default LanguageHero;
