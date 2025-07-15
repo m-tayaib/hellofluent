2f0881c61ef8fe830fc233d9d4fd417c12dc3591
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-07-15 12:54:44 +0500

    add path to navigate for language route

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom"
 function Hero() {
     return (
         <section className="md:min-h-[50vh] px-8 md:px-16 py-6">
@@ -11,9 +12,12 @@ function Hero() {
                     <p className="text-base   sm:text-lg md:text-xl text-gray-600 mb-6 max-w-xl leading-relaxed transition-all duration-300">
                         Grow your fluency with consistent practice — one word, one phrase, one day at a time.
                     </p>
-                    <button className="py-3 px-6 rounded-lg mt-2 bg-white/70 text-primary font-semibold shadow-lg hover:bg-primary hover:text-white transition-colors duration-300 ease-in-out cursor-pointer backdrop-blur-sm border border-primary/20">
+                    <Link
+                        to="/languages"
+                        className="py-3 px-6 rounded-lg mt-2 bg-white/70 text-primary font-semibold shadow-lg hover:bg-primary hover:text-white transition-colors duration-300 ease-in-out cursor-pointer backdrop-blur-sm border border-primary/20 inline-block"
+                    >
                         Start Learning
-                    </button>
+                    </Link>
                 </div>
                 {/* image content container  */}
                 <div className="md:order-2 flex justify-end items-center order-1 md:w-full md:h-full w-[250px] h-[350px] mx-auto">
